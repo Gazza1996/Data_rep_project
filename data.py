7943a1d05aebb40552c473065ffbeb717c81bc17
Gazza1996 <gkmannion@hotmail.com>
2016-11-23 20:19:35 +0000

    Commit

--- data.py
+++ data.py
@@ -1,5 +1,16 @@
 from flask import Flask, render_template
+from flask.ext.mysql import MySQL
+#from werkzeug import generate_password_hash, check_password_hash
+
+mysql = MySQL()
 app = Flask(__name__)
+app.secret_key= "G00319609"
+
+# SQL configuration
+app.config['MYSQL_DATABASE_USER'] = 'gary'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'gary'
+app.config['MYSQL_DATABASE_DB'] = 'bucketlist'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
 @app.route("/")
 def main():
