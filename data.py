5d7adae747d6e2ce0414bc26666c2e6184809877
Gazza1996 <gkmannion@hotmail.com>
2016-11-24 19:09:44 +0000

    Commit

--- data.py
+++ data.py
@@ -1,20 +1,20 @@
-from flask import Flask, render_template, session, redirect, request, jsonify
+from flask import Flask, render_template, json, request, redirect, session, jsonify
 from flask.ext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 
 mysql = MySQL()
 app = Flask(__name__)
-app.secret_key= "G00319609"
+app.secret_key = 'G00319609'
 
-# SQL configuration
+# MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'gary'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'gary'
-app.config['MYSQL_DATABASE_DB'] = 'bucketlist'
+app.config['MYSQL_DATABASE_DB'] = 'bucketList'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-# calls the index.html file
-@app.route("/")
+# calls index.html
+@app.route('/')
 def main():
     return render_template('index.html')
 
@@ -22,6 +22,11 @@ def main():
 @app.route('/showSignUp')
 def showSignUp():
     return render_template('signup.html')
+
+# calls bucketList.html
+@app.route('/showBucketLIst')
+def showAddWish():
+    return render_template('bucketList.html')
 
 # calls signin.html and userLink.html
 @app.route('/showSignin')
@@ -31,43 +36,88 @@ def showSignin():
     else:
         return render_template('signin.html')
 
-# calls bucketList.html
-@app.route('/showBucketList')
-def showBucketList():
-    return render_template('bucketList.html')
-
-# @app.route('/logout')
-#def logout():
- #   session.pop('user',None)
-  #  return redirect('/')
-
 # calls userLink.html
 @app.route('/userLink')
 def userHome():
     if session.get('user'):
         return render_template('userLink.html')
 
+#@app.route('/logout')
+#def logout():
+ #   session.pop('user', None)
+  #  return redirect('/')
+
+# calls to get a wish
 @app.route('/getWish')
 def getWish():
-        try:
-            if session.get('user'):
-                _user = session.get('user')
+    try:
+        if session.get('user'):
+            _user = session.get('user')
 
-                con = mysql.connect()
-                cursor = con.cursor()
-                cursor.callproc('sp_GetWishByUser', (_user,))
-                wishes = cursor.fetchall()
+            con = mysql.connect()
+            cursor = con.cursor()
+            cursor.callproc('sp_GetWishByUser', (_user,))
+            wishes = cursor.fetchall()
 
-                wishes_dict = []
-                for wish in wishes:
-                    wish_dict = {
-                        'Id': wish[0],
-                        'Title': wish[1],
-                        'Description': wish[2],
-                        'Date': wish[4]}
-                    wishes_dict.append(wish_dict)
+            wishes_dict = []
+            for wish in wishes:
+                wish_dict = {
+                    'Id': wish[0],
+                    'Title': wish[1],
+                    'Description': wish[2],
+                    'Date': wish[4]}
+                wishes_dict.append(wish_dict)
 
-# route to sign up method
+            return json.dumps(wishes_dict)
+        else:
+            return render_template('/')
+    except Exception as e:
+        return render_template('/', error=str(e))
+
+# calls to add a wish
+@app.route('/addWish', methods=['POST'])
+def addWish():
+    try:
+        if session.get('user'):
+            _title = request.form['inputTitle']
+            _description = request.form['inputDescription']
+            _user = session.get('user')
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_addWish', (_title, _description, _user))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return redirect('/userHome')
+    finally:
+        cursor.close()
+        conn.close()
+
+# method to validate login is correct
+@app.route('/validateLogin', methods=['POST'])
+def validateLogin():
+    try:
+        _username = request.form['inputEmail']
+        _password = request.form['inputPassword']
+
+        # connect to mysql
+
+        con = mysql.connect()
+        cursor = con.cursor()
+        cursor.callproc('sp_validateLogin', (_username,))
+        data = cursor.fetchall()
+
+        if len(data) > 0:
+            if check_password_hash(str(data[0][3]), _password):
+                session['user'] = data[0][0]
+                return redirect('/userHome')
+    finally:
+        cursor.close()
+        con.close()
+
+# validate correct sign up formats
 @app.route('/signUp', methods=['POST', 'GET'])
 def signUp():
     try:
@@ -75,10 +125,10 @@ def signUp():
         _email = request.form['inputEmail']
         _password = request.form['inputPassword']
 
-        # validate name, email and password
+        # validate the received values
         if _name and _email and _password:
 
-            # call MySQL
+            # All Good, let's call MySQL
 
             conn = mysql.connect()
             cursor = conn.cursor()
@@ -89,17 +139,10 @@ def signUp():
             if len(data) is 0:
                 conn.commit()
                 return json.dumps({'message': 'User created successfully !'})
-            else:
-                return json.dumps({'error': str(data[0])})
-        else:
-            return json.dumps({'html': '<span>Enter the required fields</span>'})
-
-    except Exception as e:
-        return json.dumps({'error': str(e)})
     finally:
         cursor.close()
         conn.close()
 
-
+# method to run application
 if __name__ == "__main__":
     app.run()
